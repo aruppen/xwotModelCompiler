25b3c55c85db0d5e5b6bf45aed9d9e64e7b5bd29
Andreas Ruppen <andreas.ruppen@gmail.com>
2014-10-01 10:54:21 +0200

    Working version which makes differences between two types of Devices (EntityDevices and NodeDevices) to later distinguish how many services need to be created

--- src/physical2virtualEntities.py
+++ src/physical2virtualEntities.py
@@ -88,8 +88,8 @@ class Physical2VirtualEntities:
         ve.setAttribute('name', name + 'Resource')
         uri = raw_input('Specify URI for ' + Fore.RED + elType + ' ' + Fore.GREEN + name + Fore.RESET + ': ')
         ve.setAttribute('uri', uri)
-        if elType == 'xwot:VDevice':
-            ve.setAttribute('xsi:type', 'xwot:VResource')
+        if elType == 'xwot:NodeDevice':
+            ve.setAttribute('xsi:type', 'xwot:NodeResource')
             root.appendChild(ve)
             physicalEntities = list(
                 self.__filterChildrenByTagName(self.__model.getElementsByTagName('PhysicalEntity')[0], 'Component'))
@@ -97,8 +97,8 @@ class Physical2VirtualEntities:
             for entity in physicalEntities:
                 self.__log.debug('Working on Node: ' + entity.getAttribute('name'))
                 self.__addResources(newPhysEnt, entity, ve)
-        elif elType == 'xwot:Device':
-            ve.setAttribute('xsi:type', 'xwot:Resource')
+        elif elType == 'xwot:EntityDevice':
+            ve.setAttribute('xsi:type', 'xwot:EntityResource')
             root.appendChild(ve)
             physicalEntities = list(
                 self.__filterChildrenByTagName(self.__model.getElementsByTagName('PhysicalEntity')[0], 'Component'))
@@ -125,16 +125,16 @@ class Physical2VirtualEntities:
         vent.setAttribute('uri', uri)
         newTargetPhysicalEntity = sourceNode.cloneNode(False)
         targetPhysicalEntity.appendChild(newTargetPhysicalEntity)
-        if etype == 'xwot:VDevice':
-            vent.setAttribute('xsi:type', 'xwot:VResource')
+        if etype == 'xwot:NodeDevice':
+            vent.setAttribute('xsi:type', 'xwot:NodeResource')
             targetNode.appendChild(vent)
             subPhysicalEntities = list(self.__filterChildrenByTagName(sourceNode, 'Component'))
             subPhysicalEntities = self.__searchForContextResources(subPhysicalEntities, newTargetPhysicalEntity, vent)
             for subEntity in subPhysicalEntities:
                 self.__log.debug('Working on Node: ' + subEntity.getAttribute('name'))
                 self.__addResources(newTargetPhysicalEntity, subEntity, vent)
-        elif etype == 'xwot:Device':
-            vent.setAttribute('xsi:type', 'xwot:Resource')
+        elif etype == 'xwot:EntityDevice':
+            vent.setAttribute('xsi:type', 'xwot:EntityResource')
             targetNode.appendChild(vent)
             subPhysicalEntities = list(self.__filterChildrenByTagName(sourceNode, 'Component'))
             subPhysicalEntities = self.__searchForContextResources(subPhysicalEntities, newTargetPhysicalEntity, vent)
