f74161358b69a251e0392d5c5fd9dc018405eeef
Andreas Ruppen <andreas.ruppen@gmail.com>
2014-10-27 10:51:58 +0100

    Added a cleanup script to remove any installed artifacts

---
+++ clean.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo rm -rf build/ dist/ XWoT_Model_Translator.egg-info/ /usr/local/lib/python2.7/dist-packages/XWoT_Model_Translator-1.1-py2.7.egg/ /usr/local/lib/python3.4/dist-packages/XWoT_Model_Translator-1.1-py3.4.egg/ /usr/local/bin/model2* /usr/local/bin/physical2virtualEntities
