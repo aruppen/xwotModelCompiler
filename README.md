6c60b9050e0efce3fc4acf5581803e7f0e5a5163
Andreas Ruppen <andreas.ruppen@gmail.com>
2015-07-31 11:36:11 +0200

    Modifications necessary to deploy on pypi

--- README.md
+++ README.md
@@ -4,7 +4,13 @@ Convert an xWoT meta-model to executable python code.
 
 # Installation
 
-## Final installation
+## End Users
+
+    pip install exaManagementSystem
+    
+## Developers
+
+### Final installation
 
 from a terminal launch
 
@@ -16,7 +22,7 @@ this will compile and install the project to the pyhton libraries (eg. /usr/loca
 * model2WADL
 The configuration and logging.conf are copied into /etc/Model2WADL/ but it is possible to overwrite them either by placing a file with the same name (but prefixed with a dot eg. .logging.conf) in the user home directory or a file with the same name in the current working directory.
 
-## Development installation
+### Development installation
 
 from a terminal launch
 
@@ -24,12 +30,12 @@ from a terminal launch
     
 does the same as before but, uses links instead of copying files.
 
-# Clean Working directory
+### Clean Working directory
 
 To clean the working directory
     
     sudo python setup.py clean --all
-    sudo rm -rf build/ dist/ XWoT_Model_Translator.egg-info/ files.txt
+    sudo rm -rf build/ dist/ xWoTModelTranslator.egg.egg-info/ files.txt
 
 
 # Uninstall
@@ -42,18 +48,21 @@ To clean the working directory
 First find the installed package with pip and the uninstall it
 
     ~/Documents/Programming/Python/Model2WADL [master|✚ 1…1] 
-    12:13 $ pip freeze |grep XWot*
-    3:XWoT-Model-Translator==1.1
+    12:13 $ pip freeze |grep xWot*
+    3:xWoTModelTranslator==1.1
     ~/Documents/Programming/Python/Model2WADL [master|✚ 1…1] 
-    12:13 $ sudo pip uninstall XWoT-Model-Translator
-    Uninstalling XWoT-Model-Translator:
-      /Library/Python/2.7/site-packages/XWoT_Model_Translator-1.1-py2.7.egg
+    12:13 $ sudo pip uninstall xWoTModelTranslator
+    Uninstalling xWoTModelTranslator:
+      /Library/Python/2.7/site-packages/xWoTModelTranslator-1.1-py2.7.egg
       /usr/local/bin/model2Python
       /usr/local/bin/model2WADL
       /usr/local/bin/physical2virtualEntities
     Proceed (y/n)? y
-        Successfully uninstalled XWoT-Model-Translator
+        Successfully uninstalled xWoTModelTranslator
      ~/Documents/Programming/Python/Model2WADL [master|✚ 1…1] 
     12:13 $
+
      
+ # Utilisation
  
+ The package provide two scripts: _physical2virtualEntities_ and _model2Python_. The first one is used to enhance an xWoT model (created with the according Eclipse plugin see https://github.com/aruppen/architecture4xwot for how to get this eclipse plug-in) containing only the virtual side of a Device with the corresponding virtual side. The second script takes a final xWoT model and translates it into code skeletons. The code sekeltons reflects the chosen hierarchy as a RESTful webservice based on python and autobahn.
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 from setuptools import setup, find_packages
 from distutils.command.install_data import install_data
+from pip.req import parse_requirements
 
 
 def walk_subpkg(name):
@@ -17,47 +18,60 @@ def walk_subpkg(name):
 scripts = ['bin/model2WADL', 'bin/model2Python', 'bin/physical2virtualEntities']
 cmdclass = {'install_data': install_data}
 data_files = [('/etc/Model2WADL/', ['etc/Model2WADL.cfg', 'etc/Physical2Virtual.cfg', 'etc/logging.conf'])]
-package_data = {'xwot': ['examples/*'],
-                "xwot": [] + walk_subpkg('REST-Server-Skeleton/') + walk_subpkg('NM_REST-Server-Skeleton/') + walk_subpkg('examples/')}
-
-# except IndexError: pass
+package_data = {"xwot": [] + walk_subpkg('REST-Server-Skeleton/') + walk_subpkg('NM_REST-Server-Skeleton/')}
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements('requirements.txt')
+reqs = [str(ir.req) for ir in install_reqs]
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
-
-
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
-    name="XWoT Model Translator",
+    name="xWoTModelTranslator",
     version="1.1",
     author="Andreas Ruppen",
     author_email="andreas.ruppen@unifr.ch",
     description="Translates xWoT models into various code snippets",
-    license="GPL",
+    license="Apache",
     keywords="WoT IoT REST Arduino xWoT",
     url="http://diufpc46.unifr.ch/projects/projects/thesis",
-    packages=find_packages(),
-    package_data=package_data,
-    scripts=scripts,
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    package_data = package_data,
+    entry_points={
+        'console_scripts': [
+            'model2Python=Model2Python:main',
+            'model2WADL=Model2WADL:main',
+            'pyhsical2virtualEntities=Physical2VirtualEntities:main'
+        ],
+    },
     cmdclass=cmdclass,
     data_files=data_files,
-    install_requires=['colorama'],
+    install_requires=reqs,
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
-        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Science/Research',
         "Topic :: Utilities",
-        "License :: OSI Approved :: GPL License",
+        'Topic :: Education',
+        'Topic :: Software Development :: Compilers',
+        "License :: OSI Approved :: Apache Software License",
         'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
     platforms='any',
 )
