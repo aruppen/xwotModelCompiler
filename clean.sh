ebf53ce80d5fe39bb1ec4cc5463f3bcd035c9c35
Andreas Ruppen <andreas.ruppen@gmail.com>
2015-09-29 14:42:53 +0200

    Updated the clean.sh script to the current release

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo rm -rf build/ dist/ XWoT_Model_Translator.egg-info/ /usr/local/lib/python2.7/dist-packages/XWoT_Model_Translator-1.1-py2.7.egg/ /usr/local/lib/python3.4/dist-packages/XWoT_Model_Translator-1.1-py3.4.egg/ /Library/Python/2.7/site-packages/xWoTModelTranslator-1.3-py2.7.egg/ /usr/local/bin/model2* /usr/local/bin/physical2virtualEntities
+sudo rm -rf build/ dist/ XWoT_Model_Translator.egg-info/ /usr/local/lib/python2.7/dist-packages/XWoT_Model_Translator-1.5-py2.7.egg/ /usr/local/lib/python3.4/dist-packages/XWoT_Model_Translator-1.5-py3.4.egg/ /Library/Python/2.7/site-packages/xWoTModelTranslator-1.5-py2.7.egg/ /usr/local/bin/model2* /usr/local/bin/physical2virtualEntities
